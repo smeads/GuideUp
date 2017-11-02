68da7033e8acca965066ea4874c2e9c6fe10571a
Steven Meads <sbmeads@gmail.com>
2017-11-02 15:38:02 -0700

    add sticky nav logic for scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-
+  $('.js--section-packages').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  })
 });
