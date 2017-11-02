981521562871c9f83bebf30558c6fc75c6d6b32b
Steven Meads <sbmeads@gmail.com>
2017-11-02 15:54:44 -0700

    add js logic for handling hero button clicks and scroll animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // Sticky Navigation
   $('.js--section-packages').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -7,5 +9,13 @@ $(document).ready(function() {
     }
   }, {
     offset: '60px;'
+  })
+
+  // Scroll on buttons
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  })
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-packages').offset().top}, 1000);
   })
 });
